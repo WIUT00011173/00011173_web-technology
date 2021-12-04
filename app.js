7e25e46fede361b29bff8aced5cf6054142b7343
WIUT00011173 <wiut00011173student@gmail.com>
2021-12-04 12:22:41 +0500

    finalizing pages, js starting

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-const body = document.querySelector("main");
-const navbar = document.querySelector(".navbar");
-const menu = document.querySelector(".menu-list");
-const menuBtn = document.querySelector(".menu-btn");
-const cancelBtn = document.querySelector(".cancel-btn");
-menuBtn.onclick = ()=>{
-  menu.classList.add("active");
-  menuBtn.classList.add("hide");
-  cancelBtn.classList.add("show");
-  body.classList.add("disabledScroll");
+const toggle = document.getElementById('toggle');
+function funksiya(){
+  toggle.classList.toggle('active')
 }
-cancelBtn.onclick = ()=>{
-  menu.classList.remove("active");
-  menuBtn.classList.remove("hide");
-  cancelBtn.classList.remove("show");
-  body.classList.remove("disabledScroll");
-}
-window.onscroll = ()=>{
-  this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
+toggle.onclick = funksiya;
